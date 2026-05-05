450ebdf7fc2c2385fe2cee0bacf8d2a72e2906ab
mhong1208 <myhong11a32004@gmail.com>
2026-05-05 23:08:01 +0700

    feat: add new col supplier

---
+++ Data/Migrations/20260505160304_AddNewColSupplier.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace price_management_api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColSupplier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Suppliers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Suppliers");
+        }
+    }
+}
