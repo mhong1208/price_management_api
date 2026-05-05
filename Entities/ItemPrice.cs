365b9edc1f7cbfc8bcd721db0252db9c6d268a2a
mhong1208 <myhong11a32004@gmail.com>
2026-05-05 13:37:53 +0700

    update: entity item, item price, supplier

--- Entities/ItemPrice.cs
+++ Entities/ItemPrice.cs
@@ -2,12 +2,12 @@ namespace price_management_api.Entities;
 
 public class ItemPrice : BaseEntity {
     public int ItemId { get; set; }
-    public Item Item { get; set; }
-    
+    public Item Item { get; set; } = null!;
+
     public int SupplierId { get; set; }
-    public Supplier Supplier { get; set; }
-    
+    public Supplier Supplier { get; set; } = null!;
+
     public decimal Price { get; set; }
-    public string Currency { get; set; }
+    public required string Currency { get; set; }
     public DateTime EffectiveDate { get; set; }
 }
--- Entities/Supplier.cs
+++ Entities/Supplier.cs
@@ -1,10 +1,10 @@
 namespace price_management_api.Entities;
 
 public class Supplier : BaseEntity {
-    public string SupplierCode { get; set; }
-    public string SupplierName { get; set; }
-    public string Email { get; set; }
+    public required string SupplierCode { get; set; }
+    public required string SupplierName { get; set; }
+    public string? Email { get; set; }
     // ... các trường khác theo đề bài
-    
-    public ICollection<ItemPrice> ItemPrices { get; set; }
+
+    public ICollection<ItemPrice> ItemPrices { get; set; } = [];
 }
